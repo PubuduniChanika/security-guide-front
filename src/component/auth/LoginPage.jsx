c2269342fd152e6026452c322d0d0bdbafea75fa
PubuduniChanika <pchanikawijesinghe@gmail.com>
2024-09-16 13:41:09 +0530

    first commit

---
+++ src/component/auth/LoginPage.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import UserService from "../service/UserService";
+function LoginPage(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const userData = await UserService.login(email, password);
+            if (userData.token){
+                localStorage.setItem("token", userData.token);
+                localStorage.setItem("role", userData.role);
+                navigate('/profile');
+            }else{
+                setError(userData.message)
+            }
+        } catch (error) {
+            console.log(error);
+            setError(error.message);
+            setTimeout(() =>{
+                setError("");
+            }, 5000);
+        }
+    }
+    return(
+        <div className="auth-container"> 
+            <h1>Login</h1>
+            {error && <p className="error-message">{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" value={email} onChange = {(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" value={password} onChange = {(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit">Login</button>
+
+            </form>
+            <p>Don't have an account? <a href="/signup">Sign Up</a></p>
+        </div>
+    )
+
+}
+
+export default LoginPage;
